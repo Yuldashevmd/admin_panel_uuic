538a985465e93785109ff9f92e0cef3fc7feade2
Muhammad <muxammad_205@mail.ru>
2023-12-01 15:47:17 +0500

    fixed bug belongs to github action

--- src/service/hooks/useData.jsx
+++ src/service/hooks/useData.jsx
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { Context } from "../Context";
+import { Context } from "src/service/context";
 
 const useData = () => {
   const [data, setData] = useContext(Context);
